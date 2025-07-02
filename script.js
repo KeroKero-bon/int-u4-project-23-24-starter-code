5f5265c7267fa1cd81651b415dee155f3c2c77ac
KeroKero-bon <thandikasingleton@gmail.com>
2025-07-02 10:00:17 -0400

    anime

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // JavaScript goes here.
+let optionOneButton = document.querySelector(".option-one");
+let optionTwoButton = document.querySelector(".option-two");
+let optionOnescreen = document.querySelector(".option-one-screen");
+let optionTwoscreen = document.querySelector(".option-two-screen");
+let optionOneEnd = document.querySelector(".option-one-end");
+let optionTwoEnd = document.querySelector("option-two-end");
+
+let optionThreeButton = document.querySelector(".option-three");
+let optionFourButton = document.querySelector(".option-four");
+let optionThreescreen = document.querySelector(".option-Three-screen");
+let optionFourscreen = document.querySelector(".option-Four-screen");
+let optionThreeEnd = document.querySelector(".option-Three-end");
+let optionFourEnd = document.querySelector("option-Four-end");
+let optionFiveButton = document.querySelector(".option-five");
+let optionSixButton = document.querySelector(".option-six");
+let optionSevenButton = document.querySelector(".option-seven");
+let optionEightButton = document.querySelector(".option-eight");
+
+
+
+
